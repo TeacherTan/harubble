3d8e4c75c8e06efd15d2a653e3816bbdd76fced5
RoyDust <3214026782@qq.com>
2026-04-17 01:52:01 +0800

    feat: add shadcn svelte primitives

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export type WithElementRef<T> = T & {
+  ref?: HTMLElement | null;
+  class?: string;
+};
+
+export type WithoutChildren<T> = Omit<T, "children">;
+
+export type WithoutChild<T> = Omit<T, "child">;
+
+export type WithoutChildrenOrChild<T> = Omit<T, "children" | "child">;
